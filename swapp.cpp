2662ee87d5844c518cab186bbbed77bb7393f42f
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-09-17 13:26:02 +0530

    Add files via upload

---
+++ swapp.cpp
@@ -0,0 +1,14 @@
+//Swapping of two number
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of A and B :";
+	cin>>a>>b;
+	a=a^b;
+	b=a^b;
+	a=a^b;
+	cout<<"value of A and B after Swapping :"<<a<<" "<<b<<endl;
+	return 0;
+}
