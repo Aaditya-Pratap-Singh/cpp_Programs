4395646d37c7d6f8500d7de603f73a472f484e66
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-09-17 13:22:00 +0530

    Add files via upload

---
+++ rev-palen.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,digit,rev=0;
+	cout<<"Enter the value of n: ";
+	cin>>n;
+	int temp=n;
+	while(n>0){
+		digit=n%10;
+		rev=(rev*10)+digit;
+		n=n/10;
+	}
+	if(rev==temp){
+		cout<<"NUMBER IS PALENDROM"<<endl;
+	}
+	else{
+		cout<<"NUMBER IS NOT PALENDROM"<<endl;
+	}
+	return 0;
+}
