8123079596167cca4572569515deda50a1ce6242
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-09-17 13:25:43 +0530

    Add files via upload

---
+++ sum_mat.cpp
@@ -0,0 +1,67 @@
+//addition of two matrix
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r,c,i,j;
+	cout<<"Enter the value of row and column :"<<endl ;
+	cin>>r>>c;
+	int a[r][c];
+	int b[r][c];
+	int sum[r][c];
+	
+	cout<<"Enter the value of first matrix :"<<endl;
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			cin>>a[i][j];
+		}
+	}
+	
+	cout<<"Enter the value of seccond matrix :"<<endl;
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			cin>>b[i][j];
+		}
+	}
+	
+	cout<<"Display the element of first matrix :"<<endl;
+	for(i=0;i<r;i++)
+    {
+    	for(j=0;j<c;j++)
+    	{
+    		cout<<" "<<a[i][j];
+		}
+		cout<<endl;
+	}
+	
+	cout<<"Display the element of second matrix :"<<endl;
+	for(i=0;i<r;i++)
+    {
+    	for(j=0;j<c;j++)
+    	{
+    		cout<<" "<<b[i][j];
+		}
+		cout<<endl;
+	}
+	
+	cout<<"Sum of matrix";
+  for(i=0;i<r;i++)
+  {
+  	for(j=0;j<c;j++)
+  	{
+  		sum[i][j]=a[i][j]+b[i][j];
+	  }
+  }
+  cout<<" Display the sum "<<endl;
+  for(i=0;i<r;i++)
+  {
+  	for(j=0;j<r;j++)
+  	{
+  		cout<<" "<<sum[i][j];
+	  }
+	  cout<<endl;
+  }
+  return 0;
+}
+
+
