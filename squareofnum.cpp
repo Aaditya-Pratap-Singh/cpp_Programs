591772890e6dd243ca5ae6bd77a3084c9f4b4649
Aaditya Pratap Singh <itsaadityasingh10@gmail.com>
2026-09-25 11:33:32 +0530

    Add files via upload

---
+++ squareofnum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double n,square;
+	cout<<"Enter a number: "<<endl;
+	cin>>n;
+	square=n*n;
+	cout<<"Square of "<<n<<"is:"<<square<<endl;
+	return 0;
+}
